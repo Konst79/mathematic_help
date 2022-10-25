f9eff700be46dd52dbed8294751f7e420da0d4ff
Konstantin <Konstantintvel@gmail.com>
2022-10-25 20:21:49 +0300

    25.10.2022

---
+++ js/script.js
@@ -0,0 +1,32 @@
+function start_anime(s_id){
+    var x1 = document.getElementById(s_id);
+    x1.beginElement();
+}
+
+function p1_AB_1(){
+    var x1 = document.getElementById('animate-p1-AB-1');
+    var x2 = document.getElementById('animate-p1-AB-1-1');
+    x1.beginElement();
+    x2.beginElement();
+}
+
+function p1_AB_2(){
+    var x1 = document.getElementById('animate-p1-AB-2');
+    var x2 = document.getElementById('animate-p1-AB-2-2');
+    x1.beginElement();
+    x2.beginElement();
+}
+
+function p1_CA_CB_1(){
+    var x = document.getElementById('animate-CA-1');
+    var y = document.getElementById('animate-CB-1');
+    x.beginElement();
+    y.beginElement();
+}
+
+function p1_CA_CB_2(){
+    var x = document.getElementById('animate-CA-2');
+    var y = document.getElementById('animate-CB-2');
+    x.beginElement();
+    y.beginElement();
+}
